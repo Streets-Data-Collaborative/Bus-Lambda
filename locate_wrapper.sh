5debae28914bf4dc00d2f33e6ace5c5b3fa19df9
Aaron D'Souza <ajd629@nyu.edu>
2019-07-04 12:14:07 -0400

    Updated Locate Wrapper

--- locate_wrapper.sh
+++ locate_wrapper.sh
@@ -5,8 +5,8 @@ do
 	timestamp=`date +%s`
 	macid=`ifconfig en1 | grep ether | awk '{print $2}'`
 
-	echo $macid,$timestamp,$location
+	echo $macid,$timestamp,'''$location'''
 	curl -s 'https://docs.google.com/forms/d/1AnW5Oj71X2CC6hyrAYb7v805JIDnCKj23xav6Okh5KY/formResponse' --data "entry.512945646=$macid" --data "entry.1966681112=$location" >>/dev/null
-	python upload_device_data_to_db.py --device_id $macid --timestamp $timestamp --location '$location'
+	python upload_device_data_to_db.py --device_id $macid --timestamp $timestamp --location "$location"
 	sleep 30
 done
