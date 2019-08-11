7d50c08e06559d0ae1ba34aeb383aceba4ea81b1
vishelar <vishelar@gmail.com>
2019-08-11 13:28:39 -0400

    distinct buses

--- findDevice_lambda.py
+++ findDevice_lambda.py
@@ -8,27 +8,30 @@ import json
 
 def lambda_handler(event, context):
     con = None
-    # querytext = ('''SELECT datetime AS recenttime, ST_X(geom) as back_lon, ST_Y(geom) as back_lat, device_id
-    #     FROM bus.device_test
-    #     WHERE ST_Distance_Sphere(geom, ST_MakePoint({0},{1})) <= 3 * 1609.34 
-    #     AND datetime >= now()::timestamp - interval '15 minutes'
-    #     ORDER BY recenttime DESC
-    #     LIMIT 20;'''.format(event['lon'], event['lat']))
+    #https://stackoverflow.com/questions/3800551/select-first-row-in-each-group-by-group
+    # querytext = ('''SELECT DISTINCT ON (device_id)
+    #  datetime AS recenttime, ST_X(geom) as back_lon, ST_Y(geom) as back_lat, device_id
+    #  FROM bus.device_test
+    #  WHERE ST_Distance_Sphere(geom, ST_MakePoint({0},{1})) <= 3 * 1609.34
+    #  AND datetime >= now()::timestamp - interval '15 minutes'
+    #  ORDER  BY device_id, recenttime DESC
+    #  LIMIT 20;'''.format(event['lon'], event['lat']))
 
-    querytext = ('''SELECT datetime AS recenttime, ST_X(geom) as back_lon, ST_Y(geom) as back_lat, device_id 
-        FROM bus.device_test
-        WHERE ST_Distance_Sphere(geom, ST_MakePoint({0},{1})) <= 3 * 1609.34 
-        AND datetime >= now()::timestamp - interval '10 days'
-        ORDER BY recenttime DESC
-        LIMIT 5;'''.format(event['lon'], event['lat']))
-
+    querytext = ('''SELECT DISTINCT ON (device_id)
+     datetime AS recenttime, ST_X(geom) as back_lon, ST_Y(geom) as back_lat, device_id
+     FROM   bus.device_test
+     WHERE ST_Distance_Sphere(geom, ST_MakePoint({0},{1})) > 0.1 * 1609.34
+     AND ST_Distance_Sphere(geom, ST_MakePoint({0},{1})) <= 10 * 1609.34
+     AND datetime >= now()::timestamp - interval '10 days'
+     ORDER  BY device_id, recenttime DESC
+     LIMIT 5;'''.format(event['lon'], event['lat']))
 
     try:
-        
-        host = "last_pass"
-        database = "last_pass"
-        user = "last_pass"
-        password = "last_pass"
+
+        host = 'last_pass'
+        database = 'last_pass'
+        user = 'last_pass'
+        password = 'last_pass'
 
         con = psycopg2.connect(host=host, database=database, user=user, password=password)
         cur = con.cursor()
@@ -47,7 +50,7 @@ def lambda_handler(event, context):
             back_lon = str(entry[1])
             back_lat = str(entry[2])
             back_device = entry[3]
-            
+
             req = requests.get(url)
             time_result = req.json()['rows'][0]['elements'][0]['duration']['text']
             response =  {'back_loc': str(back_lat)+","+str(back_lon),
